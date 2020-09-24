a28c606b848c65228a8fcee87c8cb913c175f66b
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-09-24 21:52:10 +1000

    weather text

--- assets/ticketmaster.js
+++ assets/ticketmaster.js
@@ -1,40 +1,42 @@
 $("#submitButton").on("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var keyword = $("#keywordInput").val()
-    // Create a function for ticket master API
-    // + city + "startDateTime=" + date
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=AU&" + "keyword=" + keyword + "&apikey=Gc8utzxGO1i0GSfZTsAAGFEf3xb5CDJT";
-    // var event = $(this).attr("data-event");
+  var keyword = $("#keywordInput").val();
+  // Create a function for ticket master API
+  // + city + "startDateTime=" + date
+  var queryURL =
+    "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=AU&" +
+    "keyword=" +
+    keyword +
+    "&apikey=Gc8utzxGO1i0GSfZTsAAGFEf3xb5CDJT";
+  // var event = $(this).attr("data-event");
 
-    $.ajax({
-        type: "GET",
-        url: queryURL,
-        async: true,
-        dataType: "json"
-    }).then(function (json) {
-        //Add console Log for JSON Dump
-        console.log(json);
-        $("#eventEl").text(JSON.stringify(json));
-        // Parse the response.
-        // Do other things.
+  $.ajax({
+    type: "GET",
+    url: queryURL,
+    async: true,
+    dataType: "json",
+  }).then(function (json) {
+    //Add console Log for JSON Dump
+    console.log(json);
+    //$("#eventEl").text(JSON.stringify(json));
+    // Parse the response.
+    // Do other things.
 
-        // Storing the rating data
-        //Grab responce event name
-        var eventName = json.name;
-        // Create Div to display event names on the chosen date
-        var EventDiv = $("<div class='movie'>");
-        var pEvent = $("<p>").text("Event Name: " + eventName);
+    // Storing the rating data
+    //Grab responce event name
+    var eventName = json.name;
+    // Create Div to display event names on the chosen date
+    var EventDiv = $("<div class='movie'>");
+    var pEvent = $("<p>").text("Event Name: " + eventName);
 
-        // Display List of Event Names
-        EventDiv.append(pEvent);
-    });
-
+    // Display List of Event Names
+    EventDiv.append(pEvent);
+  });
 });
 
 $("#clearButton").on("click", function (event) {
-
-    //Empty form on button click
-    event.preventDefault();
-    $("form").empty();
+  //Empty form on button click
+  event.preventDefault();
+  $("form").empty();
 });
--- assets/weather.js
+++ assets/weather.js
@@ -17,25 +17,59 @@ console.log(day1);
 console.log(day5);
 
 $("#submitButton").on("click", function (e) {
-    e.preventDefault();
-    console.log("button clicked");
-    var city = $("#citySelect").children("option:selected").val();
-    var date = $("#dateInput").val();
-    var keywords = $("#keywordInput").val();
-    var APIkey = "406718fbed1888cdf91f422159a0c803";
+  e.preventDefault();
+  console.log("button clicked");
+  var city = $("#citySelect").children("option:selected").val();
+  var APIkey = "406718fbed1888cdf91f422159a0c803";
 
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}`;
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}`;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    }).then(function (response) {
-        $("#weatherEl").text(JSON.stringify(response));
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    //$("#weatherEl").text(JSON.stringify(response));
 
-        console.log(queryURL);
-        console.log(response.list[7].main.temp);
-        var test = $('input[name=datePicker]:checked', '.form-group').val()
-        console.log(test)
-    });
+    console.log(queryURL);
+    console.log(response.list[7].main.temp);
+    var dayChoice = $("input[name=datePicker]:checked", ".form-group").val();
+    console.log(dayChoice);
 
-});;
+    if (dayChoice === "day1") {
+      $("#weatherEl").text(
+        "The weather on " +
+          day1 +
+          " is: " +
+          response.list[7].weather[0].description
+      );
+    } else if (dayChoice === "day2") {
+      $("#weatherEl").text(
+        "The weather on " +
+          day2 +
+          " is: " +
+          response.list[16].weather[0].description
+      );
+    } else if (dayChoice === "day3") {
+      $("#weatherEl").text(
+        "The weather on " +
+          day3 +
+          " is: " +
+          response.list[14].weather[0].description
+      );
+    } else if (dayChoice === "day4") {
+      $("#weatherEl").text(
+        "The weather on " +
+          day4 +
+          " is: " +
+          response.list[22].weather[0].description
+      );
+    } else if (dayChoice === "day5") {
+      $("#weatherEl").text(
+        "The weather on " +
+          day5 +
+          " is: " +
+          response.list[30].weather[0].description
+      );
+    }
+  });
+});
